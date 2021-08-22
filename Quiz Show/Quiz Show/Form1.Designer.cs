fb87091dedf0ed3793588f6a5fdbb304cd8a750f
whitebane <kaankandemir2000@gmail.com>
2021-08-22 14:22:57 +0300

    QuizShowWithDb
    Code Added..

---
+++ Quiz Show/Quiz Show/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace Quiz_Show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPoint = new System.Windows.Forms.Label();
+            this.lblTrue = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTick = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(91, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(457, 134);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(208, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(589, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 41);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gray;
+            this.button3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(348, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 41);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "D";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gray;
+            this.button4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(208, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 41);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "C";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gray;
+            this.button5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(348, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(68, 41);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "B";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(599, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Point :";
+            // 
+            // lblPoint
+            // 
+            this.lblPoint.AutoSize = true;
+            this.lblPoint.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPoint.Location = new System.Drawing.Point(653, 96);
+            this.lblPoint.Name = "lblPoint";
+            this.lblPoint.Size = new System.Drawing.Size(17, 19);
+            this.lblPoint.TabIndex = 2;
+            this.lblPoint.Text = "0";
+            // 
+            // lblTrue
+            // 
+            this.lblTrue.AutoSize = true;
+            this.lblTrue.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTrue.Location = new System.Drawing.Point(599, 177);
+            this.lblTrue.Name = "lblTrue";
+            this.lblTrue.Size = new System.Drawing.Size(0, 19);
+            this.lblTrue.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(1, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quest -";
+            // 
+            // lblTick
+            // 
+            this.lblTick.AutoSize = true;
+            this.lblTick.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTick.Location = new System.Drawing.Point(53, 15);
+            this.lblTick.Name = "lblTick";
+            this.lblTick.Size = new System.Drawing.Size(17, 19);
+            this.lblTick.TabIndex = 4;
+            this.lblTick.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_2);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(653, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(599, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Time :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTick);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPoint);
+            this.Controls.Add(this.lblTrue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Quiz Show";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPoint;
+        private System.Windows.Forms.Label lblTrue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Quiz Show/Quiz Show/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Quiz_Show
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-0RV7KDT\SQLEXPRESS;Initial Catalog=QuizShow;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            lblTrue.Visible = false;
+            lblTick.Visible = false;
+        }
+
+        int tick = 0;
+        int point = 0;
+        int time = 20;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            time = 20;
+            button1.Enabled = true;
+            button3.Enabled = true;
+            button5.Enabled = true;
+            button4.Enabled = true;
+            btnStart.Enabled = false;
+            btnStart.Text = "Next";
+            tick++;
+            lblTick.Text = tick.ToString();
+            lblTick.Visible = true;
+            button1.BackColor = Color.Gray;
+            button4.BackColor = Color.Gray;
+            button3.BackColor = Color.Gray;
+            button5.BackColor = Color.Gray;
+
+            if (tick==1)
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from Quest1 order by NEWID()", conn);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    button1.Text = (rdr["a"].ToString());
+                    button5.Text = (rdr["b"].ToString());
+                    button4.Text = (rdr["c"].ToString());
+                    button3.Text = (rdr["d"].ToString());
+                    textBox1.Text = (rdr["Quest"].ToString());
+                    lblTrue.Text = (rdr["true"].ToString());
+                }
+
+                conn.Close();
+            }
+
+            if (tick==2)
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from Quest2 order by NEWID()", conn);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    button1.Text = (rdr["a"].ToString());
+                    button5.Text = (rdr["b"].ToString());
+                    button4.Text = (rdr["c"].ToString());
+                    button3.Text = (rdr["d"].ToString());
+                    textBox1.Text = (rdr["Quest"].ToString());
+                    lblTrue.Text = (rdr["true"].ToString());
+                }
+
+                conn.Close();
+            }
+
+            if (tick==3)
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from Quest3 order by NEWID()", conn);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    button1.Text = (rdr["a"].ToString());
+                    button5.Text = (rdr["b"].ToString());
+                    button4.Text = (rdr["c"].ToString());
+                    button3.Text = (rdr["d"].ToString());
+                    textBox1.Text = (rdr["Quest"].ToString());
+                    lblTrue.Text = (rdr["true"].ToString());
+                }
+
+                btnStart.Text = "Game Over";
+
+                conn.Close();
+            }
+
+
+            if (tick==4)
+            {
+                btnStart.Text = "Game Over";
+                button4.Text = "";
+                button1.Text = "";
+                button5.Text = "";
+                button3.Text = "";
+                textBox1.Text = "";
+                button4.Enabled=false;
+                button5.Enabled = false;
+                button3.Enabled = false;
+                button1.Enabled = false;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            button3.Enabled = false;
+            button5.Enabled = false;
+            button4.Enabled = false;
+            if (button1.Text==lblTrue.Text)
+            {
+                point = point + 10;
+                lblPoint.Text = point.ToString();
+                button1.BackColor = Color.Green;
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                btnStart.Enabled = true;
+                button3.Enabled = false;
+                button1.Enabled = false;
+                button4.Enabled = false;
+                if (button5.Text == lblTrue.Text)
+                {
+                    point = point + 10;
+                    lblPoint.Text = point.ToString();
+                    button5.BackColor = Color.Green;
+                }
+                else
+                {
+                    button5.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == lblTrue.Text)
+            {
+                point = point + 10;
+                lblPoint.Text = point.ToString();
+                button4.BackColor = Color.Green;
+            }
+            else
+            {
+                button4.BackColor = Color.Red;
+            }
+            
+            btnStart.Enabled = true;
+            button3.Enabled = false;
+            button5.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == lblTrue.Text)
+            {
+                point = point + 10;
+                lblPoint.Text = point.ToString();
+                button3.BackColor = Color.Green;
+            }
+
+            else
+            {
+                button3.BackColor = Color.Red;
+            }
+            btnStart.Enabled = true;
+            button1.Enabled = false;
+            button5.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_2(object sender, EventArgs e)
+        {
+            time = time - 1;
+            label3.Text = time.ToString();
+
+            if (time==0)
+            {
+                MessageBox.Show("Time is over");
+                button1.Enabled = false;
+                button3.Enabled = false;
+                button5.Enabled = false;
+                button4.Enabled = false;
+                textBox1.Text = "";
+                btnStart.Enabled = true;
+
+            }
+        }
+    }
+}
